0091e5a1ed06bdcebc58d7fa13ee38fc0ebbbbc4
Nasty G <skalakul@gmail.com>
2020-11-14 10:28:12 +0100

    btn-like

--- src/components/atoms/IconFavorite.js
+++ src/components/atoms/IconFavorite.js
@@ -9,7 +9,7 @@ const IconFavorite = props => {
     };
 
     return (
-        <i className= {`${like? "fa fa-heart":"heart outline icon"} u-font-btn-l u-m-left-small`}
+        <i className= {`${like? "fa fa-heart ":"heart outline icon "} u-font-btn-l u-m-left-small`}
         style = {{color: like? "#8B0000" : "black"}}
         onClick={toggleIcon}></i>
     )
